84ac0e9fc012b56e6e81a2bd75c06dcd6ac252fb
robotgranata <eldarman@list.ru>
2019-03-21 13:52:08 +0300

    add function makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 // PLEASE DON'T change function name
+//makeExchange(49);
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    let coints = {};
+    let bank = {
+        H: 50,
+        Q: 25,
+        D: 10,
+        N: 5,
+        P: 1
+    };
+
+    if (currency > 10000 ) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    
+    for (let nominal in bank) {
+        if (bank[nominal] > currency) continue;
+        coints[nominal] = Math.floor( currency / bank[nominal]);
+        currency = currency - coints[nominal] * bank[nominal];
+    }
+    
+    return coints;
 }
